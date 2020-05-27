1142ced7ccbb29b8ba9cc5e8699d5d5f9f0715c9
Vince Urag <vinceuragvfx@gmail.com>
2020-05-27 17:25:11 +0800

    Update deployment

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :vincy, VincyWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "vin.cy", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   code_reloader: false
